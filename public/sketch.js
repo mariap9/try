f54e934deb0561f96d8c6d9e0b807901599f1aaa
maria paula <mp.vargast79@gmail.com>
2020-02-09 23:51:08 +0100

    update

--- public/sketch.js
+++ public/sketch.js
@@ -1,28 +1,53 @@
+// ITP Networked Media, Fall 2014
+// https://github.com/shiffman/itp-networked-media
+// Daniel Shiffman
+
+// Keep track of our socket connection
 var socket;
 
 function setup() {
-  createCanvas(600, 400);
-  background(51);
-
-  socket = io();
-  socket = socket.io.connect('http://localhost:3000');
-}
-
-function mouseDragged() {
-  console.log('sending:' + mouseX + ',' + mouseY);
-
-  var data = {
-    x: mouseX,
-    y: mouseY
-  }
-  socket.emit('mouse', data);
-
-  noStroke();
-  fill(255);
-  ellipse(mouseX, mouseY, 60);
+  createCanvas(400, 400);
+  background(0);
+  // Start a socket connection to the server
+  // Some day we would run this server somewhere else
+  socket = io.connect('http://localhost:3000');
+  // We make a named event called 'mouse' and write an
+  // anonymous callback function
+   socket.on('mouse', newDrawing);
+    // When we receive data
+    function newDrawing(data) {
+      console.log("Got: " + data.x + " " + data.y);
+      // Draw a blue circle
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x, data.y, 20, 20);
+    }
 }
 
 function draw() {
+  // Nothing
+}
 
+function mouseDragged() {
+  // Draw some white circles
+  fill(255);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+  // Send the mouse coordinates
+  sendmouse(mouseX,mouseY);
+}
 
+// Function for sending to the socket
+function sendmouse(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse: " + xpos + " " + ypos);
+
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mouse',data);
 }
